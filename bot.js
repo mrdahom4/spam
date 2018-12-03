91dd534766e449f0798e5df5a82e0fc8093ffcb9
Mr DaHoM <44880953+mrdahom4@users.noreply.github.com>
2018-12-03 12:27:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,4 +40,17 @@ client.on('message', message => {
           }
     });
 
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Only DaHoM.`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+
+});
+
 client.login(process.env.BOT_TOKEN);
